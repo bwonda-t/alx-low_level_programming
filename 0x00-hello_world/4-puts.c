1e5f2a15da11556887cda2ab5ec77674b475ede1
bwonda-t <Bwonda65@gmail.com>
2023-10-13 00:29:26 -0700

    mam africa

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdib.h>
+/**
+* main -A program that prints with puts function
+* Return: 0 (success)
+*/
+int main (void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
